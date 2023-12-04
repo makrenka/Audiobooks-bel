6b028160041937078e5dd05a5b77e588600e1692
makrenka <smokrenko79@gmail.com>
2023-12-04 18:47:56 +0300

    response data from db

--- server/index.js
+++ server/index.js
@@ -1,29 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-// const { AUDIOBOOKS } = require("./audiobooks");
 const Sequelize = require("sequelize");
 const db = require("./db");
-
-function t01() {
-  db.audiobooks.findAll().then((res) => {
-    console.log(res);
-  });
-}
-// t01();
-
-async function t02() {
-  const result = await db.audiobooks.findAll();
-  console.log(result[0]);
-}
-t02();
 
 const app = express();
 
 app.use(cors());
 
-// app.get("/api/books", (req, res) => {
-//   res.json(AUDIOBOOKS);
-// });
+app.get("/api/books", async (req, res) => {
+  const result = await db.audiobooks.findAll();
+  res.json(result);
+});
 
 const PORT = process.env.PORT || 3001;
 
